d90e6460cf8e46b0ec1cc887d69c61cfc7f89b1e
AmirHossein-nr <arazlighi@gmail.com>
2022-02-06 22:44:33 +0330

    config settings of firebase

---
+++ web-app/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+export const authentication = firebase.initializeApp({
+    apiKey: "AIzaSyDEp4owuOaWFXbQSClhCukEV7OtopZFONg",
+    authDomain: "yumgram-b71ed.firebaseapp.com",
+    projectId: "yumgram-b71ed",
+    storageBucket: "yumgram-b71ed.appspot.com",
+    messagingSenderId: "949769257528",
+    appId: "1:949769257528:web:61870ad39c5b36a4c25ccd"
+}).auth();
