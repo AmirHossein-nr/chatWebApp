9d0ab34e4a0688ed718f1cbec44dda6f655cdccd
AmirHossein-nr <arazlighi@gmail.com>
2022-02-06 23:09:34 +0330

    connecting Login Component to firebase

--- web-app/src/components/Login.js
+++ web-app/src/components/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { authentication } from "../firebase";
 
 //Icons 
 import google from "../assets/google.svg";
@@ -11,7 +13,8 @@ const Login = () => {
         <div className={styles.loginPage}>
             <div className={styles.loginCard}>
                 <h2> Welcome to YumGram !!</h2>
-                <div className={styles.button}>
+                <div className={styles.button}
+                    onClick={() => authentication.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
                     <img src={google} alt="google" /> Sign in with google
                 </div>
             </div>
